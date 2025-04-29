0602f5049a2b6f894b89b043e6ef5418042ff411
phuongthanh05 <161005916+phuongthanh05@users.noreply.github.com>
2025-04-29 10:28:54 +0700

    Add files via upload

---
+++ Models/ModelProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace BTVN.Models
+{
+    public class ModelProduct
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,23 @@
+﻿using BTVN.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<ModelProduct> products = new List<ModelProduct>
+            {
+               new ModelProduct { Id = 1, Name = "Laptop Lenovo ThinkPad X1", Price = 2999.99m },
+               new ModelProduct { Id = 2, Name = "Laptop dell", Price = 299m },
+               new ModelProduct { Id = 3, Name = "Laptop macbook", Price = 3000.99m }
+            };
+        public IActionResult Detail()
+        {
+            return View(products);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
